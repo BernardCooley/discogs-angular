7dc6a8a3fff275a32aa9de6ac3cb673ef9fb16de
Bernard Cooley <BCooley@macmillan.org.uk>
2018-04-09 14:19:03 +0100

    data added

--- src/app/services/releases/releases.service.ts
+++ src/app/services/releases/releases.service.ts
@@ -92,6 +92,143 @@ export class ReleasesService {
           "proben",
           "EricTarlouf"
         ]
+      },
+      {
+        "releaseName": "Various ‎– More Space To Dance",
+        "releaseId": "182735", 
+        "sellers":
+        [
+          "1207vinyl.dk",
+          "Raw_Torino",
+          "PlexusRecords",
+          "SYNCHRONICITIES",
+          "brightonvinylfinds",
+          "riouxs",
+          "basement.5",
+          "MILLSPEED",
+          "moment_in_time",
+          "1207vinyl.dk",
+          "Andale23"
+        ]
+      },
+      {
+        "releaseName": "Galaxy 2 Galaxy ‎– Galaxy 2 Galaxy",
+        "releaseId": "4276", 
+        "sellers":
+        [
+          "biragomes",
+          "BRUCE1991",
+          "SLIMM893",
+          "REALMS",
+          "biragomes",
+          "mcas4150",
+          "ElstredVaump",
+          "sounzdog",
+          "eshi",
+          "tiagolobo",
+          "mondo_diggers_club",
+          "jd221169",
+          "clone.nl",
+          "Swinger",
+          "ddrthe1",
+          "SoundChamber",
+          "gyuly",
+          "dansauk",
+          "KillaCutz",
+          "MariaRecord",
+          "TF2036",
+          "mazegogroove",
+          "houseofvinyl.london",
+          "VISTI",
+          "tashi.delek",
+          "AndreaMazzucato",
+          "djrobv",
+          "Integer",
+          "interstellarsounds"
+        ]
+      },
+      {
+        "releaseName": "Various ‎– Decade Dubs",
+        "releaseId": "8440968", 
+        "sellers":
+        [
+          "Mimen",
+          "OBIU",
+          "Vinyl_In_Me",
+          "greytail",
+          "private",
+          "Teresa"
+        ]
+      },
+      {
+        "releaseName": "Golden Girls ‎– Kinetic",
+        "releaseId": "10785", 
+        "sellers":
+        [
+          "jenzi54",
+          "shoobeedoo",
+          "VandalG",
+          "unsitrovapiunulla",
+          "sugarsoundz",
+          "SeinMaedchen",
+          "ivanblack",
+          "oakantheath1",
+          "Silverplatters",
+          "cieslukmusic",
+          "coxbox",
+          "djclassix",
+          "Frank-HSJ",
+          "dirk_djaydee",
+          "Dyama",
+          "XyTeC",
+          "hipcity",
+          "luca7840",
+          "MEGA-MIX"
+        ]
+      },
+      {
+        "releaseName": "Joey Beltram ‎– The Beltram Re-Releases 1989-1991",
+        "releaseId": "1952", 
+        "sellers":
+        [
+          "Technik1210",
+          "purple_vinylfunk_76",
+          "Recordresidence",
+          "laurence",
+          "Aerosolid",
+          "KillaCutz",
+          "Deskjet",
+          "Soulmotion_Dj",
+          "3345vinylrecords",
+          "VinceWatson",
+          "IllianaMusic",
+          "mrformic",
+          "MonsieurEdd",
+          "mondo_diggers_club",
+          "japhy",
+          "mrformic",
+          "urbanvinyl",
+          "mazegogroove",
+          "jeffswing",
+          "dicabor",
+          "franky.b",
+          "DJ_Angus"
+        ]
+      },
+      {
+        "releaseName": "Endlec ‎– Linear Authority",
+        "releaseId": "8563097", 
+        "sellers":
+        [
+          "www.hhv.de",
+          "weeklywax",
+          "hotmix-recordstore",
+          "thevinylcurtain",
+          "Teresa",
+          "mutt_discogs",
+          "sinewave93",
+          "TheSoundArcade"
+        ]
       }
     ]
   }
